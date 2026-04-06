7bbb07e44d2377c1368e5b7ec15266c9ffee8b8c
Link4Electronics <link4electronics@gmail.com>
2026-04-06 02:45:23 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/256x256/apps/fs-uae-launcher.png
 export DESKTOP=/usr/share/applications/fs-uae-launcher.desktop
 export STARTUPWMCLASS=fs-uae-launcher
-export DEPLOY_PYTHON=1
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
